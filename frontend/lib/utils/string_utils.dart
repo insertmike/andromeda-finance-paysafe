e68c11bb9cf924d2f98ee705b5812468f30cac48
insertmike <myonchev99@gmail.com>
2020-10-03 14:01:38 +0300

    Added string utils to validate email && password

---
+++ frontend/lib/utils/string_utils.dart
@@ -0,0 +1,26 @@
+bool isValidEmail(String value) {
+  Pattern pattern =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  RegExp regex = new RegExp(pattern);
+  return regex.hasMatch(value);
+}
+
+bool isPasswordCompliant(String password) {
+  int minLength = 8;
+  if (password == null || password.isEmpty) {
+    return false;
+  }
+
+  bool hasUppercase = password.contains(new RegExp(r'[A-Z]'));
+  bool hasDigits = password.contains(new RegExp(r'[0-9]'));
+  bool hasLowercase = password.contains(new RegExp(r'[a-z]'));
+  bool hasSpecialCharacters =
+      password.contains(new RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+  bool hasMinLength = password.length > minLength;
+
+  return hasDigits &
+      hasUppercase &
+      hasLowercase &
+      hasSpecialCharacters &
+      hasMinLength;
+}
