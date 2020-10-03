962ed6437e714b4983a300b2421a0ae248cf6461
Dimitar Pavlovski <dpavlovski@vmware.com>
2020-10-03 21:12:41 +0300

    changed the json response from /login

--- Backend/main.py
+++ Backend/main.py
@@ -55,8 +55,6 @@ def verify_password(username, password):
              response - 201
             
 """
-
-
 @app.route('/parent/<int:id>/kid', methods=['POST'])
 def register_kid(id):
     try:
@@ -96,8 +94,6 @@ def register_kid(id):
 
              response - 201
 """
-
-
 @app.route('/parent', methods=['POST'])
 def register_parent():
     try:
@@ -132,14 +128,14 @@ def register_parent():
                 
               response - 201
 """
-
-
 @app.route('/parent/<int:parent_id>/kid/<int:kid_id>/task', methods=['POST'])
 @auth.login_required
 def register_task(kid_id):
     try:
         conn = sqlite3.connect('/Users/dpavlovski/Desktop/paysafe-hackathon-vmv/Backend/Kidromeda.db')
         c = conn.cursor()
+
+        #add checking for parent permision to change the kid( is this the parent of the kid)
 
         summary = request.json.get('summary')
         reward = request.json.get('reward')
@@ -169,10 +165,11 @@ def register_task(kid_id):
             {
                 "is_parent": "VALUE",
                 "name": "VALUE"
+                "email":"VALUE",
+                "current_id":"VALUE"
+                
             }
 """
-
-
 @app.route('/login', methods=['GET'])
 @auth.login_required
 def login():
@@ -181,21 +178,24 @@ def login():
     conn = sqlite3.connect('/Users/dpavlovski/Desktop/paysafe-hackathon-vmv/Backend/Kidromeda.db')
     c = conn.cursor()
 
-    query = "SELECT NAME FROM Kid WHERE EMAIL = '" + email + "'"
+    query = "SELECT NAME, ID FROM Kid WHERE EMAIL = '" + email + "'"
     c.execute(query)
     keep = c.fetchone()
     if keep is not None:
         name = keep[0]
-        response = {'name': name, 'is_parent': False}
+        id = keep[1]
+
+        response = {'name': name, 'is_parent': False, 'email': email, 'current_id':id}
 
         return make_response(jsonify(response), 200)
 
-    query = "SELECT NAME FROM Parent WHERE EMAIL = '" + email + "'"
+    query = "SELECT NAME, ID FROM Parent WHERE EMAIL = '" + email + "'"
     c.execute(query)
     keep = c.fetchone()
     if keep is not None:
         name = keep[0]
-        response = {'name': name, 'is_parent': True}
+        id = keep[1]
+        response = {'name': name, 'is_parent': True,'email': email, 'current_id':id}
 
         return make_response(jsonify(response), 200)
 
@@ -212,12 +212,24 @@ def login():
                     
 {children: { name, balance, tasks: [{ summary, status, reward, image: string|null, comment: string }] }
 """
-
-
-@app.route('/parent/<int:id>/kid', methods=['GET'])
+@app.route('/parent/<int:parent_id>/kid', methods=['GET'])
 @auth.login_required
-def parent_ID_kids(id):
+def parent_id_kids(parent_id):
     try:
+        conn = sqlite3.connect('/Users/dpavlovski/Desktop/paysafe-hackathon-vmv/Backend/Kidromeda.db')
+        c = conn.cursor()
+        query = "SELECT ID FROM KID WHERE Parent_id = '" + str(parent_id) + "'"
+        c.execute(query)
+        keep = c.fetchone()
+        print(keep)
+
+        if keep is not None:
+            for entry in keep:
+                query = "SELECT * FROM TASK WHERE Kid_id = '" + str(entry) + "'"
+                c.execute(query)
+                keep1 = c.fetchone()
+                print(keep1)
+
         json_temp = "{}"
         temp_response = json.loads(json_temp)
         response = make_response(temp_response, 200)
