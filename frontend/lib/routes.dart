7256a991f0492f1cf7c3465676c05a01aa114567
insertmike <myonchev99@gmail.com>
2020-10-03 21:10:51 +0300

    Implemented New Task Screen

--- frontend/lib/routes.dart
+++ frontend/lib/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './screens/login_screen.dart';
 import './screens/logged_parent_screen.dart';
 import './screens/add_kid_screen.dart';
+import './screens/new_task_screen.dart';
 
 class Routes {
   static Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
